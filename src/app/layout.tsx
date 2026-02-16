026b708f7d6aa2f585e81d014060bfe2eaaffbb9
Andrew Kupriyanov <andrew.darmat@gmail.com>
2026-02-17 00:23:44 +0200

    feat: integrate Google Tag Manager (GTM) for analytics tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { LanguageProvider, type Language } from "@/lib/translations";
 import { Footer } from "@/components/layout/footer";
 import { AIKeyWarning } from "@/components/layout/ai-key-warning";
 import { getBaseUrl } from "@/lib/i18n-config";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
@@ -15,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 const SITE_URL = getBaseUrl();
-
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 // 1. Улучшенные метаданные
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
@@ -104,6 +105,28 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${GTM_ID}');
+            `,
+          }}
+        />
         <AuthProvider>
           <LanguageProvider initialLanguage={locale}>
             {children}
